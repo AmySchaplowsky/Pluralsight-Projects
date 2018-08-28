a1a911e2704eeaf2b0c7ad83a9a97669b472f3fa
Amy Schaplowsky <AMS@frdomain.fr.com>
2018-08-28 16:38:19 -0500

    adding in fake api

--- psadmin/src/api/authorData.js
+++ psadmin/src/api/authorData.js
@@ -2,16 +2,19 @@ module.exports = {
     authors:
         [
             {
+                pageId: 3,
                 id: 'cory-house',
                 firstName: 'Cory',
                 lastName: 'House'
             },
             {
+                pageId: 4,
                 id: 'scott-allen',
                 firstName: 'Scott',
                 lastName: 'Allen'
             },
             {
+                pageId: 5,
                 id: 'dan-wahlin',
                 firstName: 'Dan',
                 lastName: 'Wahlin'
